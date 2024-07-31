3d7ab81572611de35e129c91769f256b2dda99af
shanthakumar1987 <shanthakumar.e1987@gmail.com>
2024-07-31 10:33:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ provider "aws" {
   region =  "us-east-1"
   
 }
+
+
+module "dasam_first_module" {
+    source = "./demo_mod1"
+    bucket_name = "shantha-kumare"
+    var_cidr = "192.162.0.0/24"
+    var_name = "shantha-demo-vpc"
+  
+}
